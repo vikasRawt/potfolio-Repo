3ee6aed3f2e403dd3c559b60b4c71172dbf1adc4
vikas <vicky582050@gmail.com>
2023-08-20 00:10:47 +0530

    my portfolio commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links")
+    const icon = document.querySelector(".contracted-icon")
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+
+}
